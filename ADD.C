6b21c4c67cfbecbbb6afc70f8a864b0d9b414894
hatwarsarvil <107882486+hatwarsarvil@users.noreply.github.com>
2022-06-26 11:31:29 +0530

    Add files via upload

---
+++ ADD.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+
+
+       {
+	int a; int b; int c;
+
+
+
+	printf("enter a is:");
+	scanf("%d", &a);
+
+	printf("enter b:");
+	scanf("%d", &b);
+
+	c = a+b;
+
+	printf("sum is : %d", c);
+
+	getch();    //
+	clrscr();   //
+
+
+       }
