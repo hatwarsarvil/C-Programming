8613f081c07a6168ea75c1537028e25eaf45aac5
Sarvil Hatwar <107882486+hatwarsarvil@users.noreply.github.com>
2022-07-07 22:57:03 +0530

    Add files via upload

---
+++ FEETCM.C
@@ -0,0 +1,23 @@
+			     // Welcome to my program
+			// this is program is made to convert feet into cm
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+
+{
+  int a; int Result;
+
+  printf("Enter The Value Of A : ");
+  scanf("%d", &a);
+
+  Result = a*30; // this is main logic
+
+  printf("Feet Into CM Is : %d", Result);
+
+getch();
+clrscr();
+
+
+}
